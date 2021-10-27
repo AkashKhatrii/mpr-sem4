f973f65b255d86cce01729fe564507e7fc9d3a08
Akash Khatri <akashkhatri1001@gmail.com>
2021-10-28 04:19:03 +0530

    Design register page and add customer appointments functionality

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,122 +1,121 @@
-const express = require('express')
-const homeRouter = express.Router()
+const express = require("express");
+const homeRouter = express.Router();
 
-const User = require('../models/User')
-const Service = require('../models/Service')
-const Appointment = require('../models/Appointment')
+const User = require("../models/User");
+const Service = require("../models/Service");
+const Appointment = require("../models/Appointment");
 
-
-
-var city = ''
+var city = "";
+var providers = [];
 
 homeRouter.post("/", (req, res) => {
-    if(req.isAuthenticated()){
-        req.session.searchCity = req.body.city
-        res.redirect("/services/Electrician")
-    }
-    else{
-        res.redirect("/login")
-    }
+  if (req.isAuthenticated()) {
+    req.session.searchCity = req.body.city;
+    res.redirect("/services/Electrician");
+  } else {
+    res.redirect("/login");
+  }
 
-    
-    // User.find({city: city}).then((result) => {
-    //     res.render("services", {services: result})
-    // })
-    // .catch((err) => {
-    //     console.log(err);
-    // })
+  // User.find({city: city}).then((result) => {
+  //     res.render("services", {services: result})
+  // })
+  // .catch((err) => {
+  //     console.log(err);
+  // })
 
-    
-
-    // res.redirect("/")
-    
-
-})
-
+  // res.redirect("/")
+});
 
 homeRouter.get("/provider/service", (req, res) => {
-    res.render("provider")
-})
+  res.render("provider");
+});
 
 homeRouter.post("/provider/service", (req, res) => {
+  var userId = req.user.id;
+  console.log(userId);
 
-    var userId= req.user.id;
-    console.log(userId)
-
-    User.findOneAndUpdate({ _id: userId}, { $set: {profession: req.body.profession,
+  User.findOneAndUpdate(
+    { _id: userId },
+    {
+      $set: {
+        profession: req.body.profession,
         city: req.body.city,
-        experience: req.body.experience }}).then((result) => {
-            res.redirect("/home/provider")
-        })
-    // const newService = new Service({
-    //     providerName: req.body.providerName,
-    //     profession: req.body.profession,
-    //     city: req.body.city,
-    //     experience: req.body.experience
-    // })
+        experience: req.body.experience,
+      },
+    }
+  ).then((result) => {
+    res.redirect("/home/provider");
+  });
+  // const newService = new Service({
+  //     providerName: req.body.providerName,
+  //     profession: req.body.profession,
+  //     city: req.body.city,
+  //     experience: req.body.experience
+  // })
 
-    // newService.save()
-    // res.redirect("/")
-})
+  // newService.save()
+  // res.redirect("/")
+});
 
-homeRouter.post("/provider/login", (req, res) => {
-
-})
+homeRouter.post("/provider/login", (req, res) => {});
 
 homeRouter.get("/", (req, res) => {
-    // console.log(req.user.id)
-    if(req.isAuthenticated()){
-    res.render('home', { loggedIn: true})
-    }
-    else{
-        res.render('home', { loggedIn: false})
-
-    }
-})
-
+  // console.log(req.user.id)
+  if (req.isAuthenticated()) {
+    res.render("home", { loggedIn: true });
+  } else {
+    res.render("home", { loggedIn: false });
+  }
+});
 
 homeRouter.get("/home/provider", (req, res) => {
+  if (req.isAuthenticated()) {
+    console.log(req.user);
+    Appointment.find({ providerId: req.user.id, done: false }).then(
+      (appointments) => {
+        // console.log(result)
+        User.findOne({ _id: req.user.id }).then((user) => {
+          res.render("providerHome", {
+            appointments: appointments,
+            user: user,
+          });
+        });
+      }
+    );
+  } else {
+    res.redirect("/");
+  }
+});
 
-   
-    if(req.isAuthenticated()){
-        console.log(req.user)
-        Appointment.find({ providerId: req.user.id, done: false}).then(( appointments ) => {
-            // console.log(result)
-            User.findOne({ _id: req.user.id}).then((user) => {
-                
-                res.render("providerHome", { appointments: appointments, user: user })
-            })
-            
-            
-        })
-    }
-    else{
-        res.redirect("/")
-    }
-        
-    
-})
+homeRouter.get("/user/appointments", async (req, res) => {
+  var result = await Appointment.find({ userId: req.user.id });
 
-homeRouter.get("/user/appointments", (req, res) => {
+  for (const item of result) {
+    var user = await User.findOne({ _id: item.providerId });
+    providers.push(user);
+  }
+  console.log(result);
+  res.render("userAppointment", { result: result, providers: providers });
 
-    // Appointment.find({userId: req.user.id}).then((result) => {
-    //     if(result.length > 0){
-    //     User.findOne({_id: result.providerId}).then((user) =>{
-    //         console.log(user)
-    //         res.render("userAppointment", { result: result, name: user.custname, contact: user.contact})
-    //     })}
-    //     else{
-    //         res.render("userAppointment", {result: []})
-    //     }
-        
-    // })
-    res.render("userAppointment")
-})
+  // if(result.length > 0){
+  //     var i = 0;
+  // User.findOne({_id: result[i].providerId}).then((user) =>{
+  //     console.log(user)
+  //     res.render("userAppointment", { result: result, name: user.custname, contact: user.contact})
+  // })}
+  // else{
+  //     res.render("userAppointment", {result: []})
+  // }
+
+  // .catch((err) => {
+
+  // })
+});
 
 homeRouter.get("/users", (req, res) => {
-    User.find({}).then((user) => {
-        res.send(`${user[0].username}`)
-    })
-})
+  User.find({}).then((user) => {
+    res.send(`${user[0].username}`);
+  });
+});
 
-module.exports = { city, homeRouter};
+module.exports = { city, homeRouter };
