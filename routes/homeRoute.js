e14566fac215bfb35abcde00a6a619354576eb07
AkashKhatrii <akashkhatri1001@gmail.com>
2024-07-15 09:35:11 -0600

    Bug fixes

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -17,45 +17,31 @@ homeRouter.post("/", (req, res) => {
     res.redirect("/login");
   }
 
-  // User.find({city: city}).then((result) => {
-  //     res.render("services", {services: result})
-  // })
-  // .catch((err) => {
-  //     console.log(err);
-  // })
-
-  // res.redirect("/")
 });
 
 homeRouter.get("/provider/service", (req, res) => {
   res.render("provider");
 });
 
-homeRouter.post("/provider/service", (req, res) => {
-  var userId = req.user.id;
-  console.log(userId);
+homeRouter.post("/provider/service", async (req, res) => {
+  try {
+    const userId = req.user.id;
+    await User.findOneAndUpdate(
+      { _id: userId },
+      {
+        $set: {
+          profession: req.body.profession,
+          city: req.body.city,
+          experience: req.body.experience,
+        },
+      }
+    );
 
-  User.findOneAndUpdate(
-    { _id: userId },
-    {
-      $set: {
-        profession: req.body.profession,
-        city: req.body.city,
-        experience: req.body.experience,
-      },
-    }
-  ).then((result) => {
     res.redirect("/home/provider");
-  });
-  // const newService = new Service({
-  //     providerName: req.body.providerName,
-  //     profession: req.body.profession,
-  //     city: req.body.city,
-  //     experience: req.body.experience
-  // })
-
-  // newService.save()
-  // res.redirect("/")
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 homeRouter.post("/provider/login", (req, res) => {});
@@ -89,32 +75,27 @@ homeRouter.get("/home/provider", (req, res) => {
 });
 
 homeRouter.get("/user/appointments", async (req, res) => {
-  const result = await Appointment.find({ userId: req.user.id });
-  for (const item of result) {
-    const user = await User.findOne({ _id: item.providerId });
-    providers.push(user);
+  try {
+    const result = await Appointment.find({ userId: req.user.id });
+    const providers = await Promise.all(result.map(async (item) => {
+      return await User.findOne({ _id: item.providerId });
+    }));
+
+    res.render("userAppointment", { result: result, providers: providers });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
   }
-  res.render("userAppointment", { result: result, providers: providers });
-
-  // if(result.length > 0){
-  //     var i = 0;
-  // User.findOne({_id: result[i].providerId}).then((user) =>{
-  //     console.log(user)
-  //     res.render("userAppointment", { result: result, name: user.custname, contact: user.contact})
-  // })}
-  // else{
-  //     res.render("userAppointment", {result: []})
-  // }
-
-  // .catch((err) => {
-
-  // })
 });
 
-homeRouter.get("/users", (req, res) => {
-  User.find({}).then((user) => {
-    res.send(`${user[0].username}`);
-  });
+homeRouter.get("/users", async(req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(`${users[0].username}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = { city, homeRouter };
